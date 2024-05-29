6636b42cdb7ccd4acebbbaf524af4b5a71352a5b
MERSEL23 <you@example.commrs.sousou23@gmail.com>
2024-05-29 01:08:55 +0100

    Fixed stuff

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "images.pexels.com",
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.resolve.alias['@lib'] = path.resolve(__dirname, 'lib');
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
