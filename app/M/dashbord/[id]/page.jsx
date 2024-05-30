37a86c4ee693c83041cc6d3401ccc2c8322950ee
MERSEL23 <you@example.commrs.sousou23@gmail.com>
2024-05-31 00:26:27 +0100

    Fixed stuff

--- app/M/dashbord/[id]/page.jsx
+++ app/M/dashbord/[id]/page.jsx
@@ -1,6 +1,6 @@
 import { updateUser } from "/app/lib/actions";
-import { updateUser } from "../../../lib/Data";
-import { fetchServices } from "../../../lib/Data";
+import { updateUser } from "../../../lib/data";
+import { fetchServices } from "../../../lib/data";
 import styles from "/app/M/ui/dashbord/singleUser/singleUser.module.css";
 import Image from "next/image";
 
